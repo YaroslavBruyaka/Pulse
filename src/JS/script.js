55d08dec6f2a8988a8c9b2105ecb6717322731cc
YaroslavBruyaka <parus.yv@mail.ru>
2025-10-09 00:38:07 +0300

    second

---
+++ src/JS/script.js
@@ -0,0 +1,17 @@
+  
+///настройка слайдера////
+const slider = tns({
+container: '.carousel__inner', //название класса слайдера
+items: 1, //кол-во слайдов на странице
+slideBy: 'page',
+autoplay: false, //автоплей выкл
+controls: false, //убираем верхние конпки (стандартные)
+nav: false, //убираем точки
+});
+
+//привязка кнопок к слайдеру
+document.querySelector('.prev').addEventListener('click', () => { //получаем элемент с классом prev и вешаем обработчик событий
+slider.goTo('prev');});
+
+document.querySelector('.next').addEventListener('click', () => { //получаем элемент с классом next и вешаем обработчик событий
+slider.goTo('next');});
